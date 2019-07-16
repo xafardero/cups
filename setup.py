972bc2acf24dbe7f64a55520c26b9a838a95fc7b
xaf <xavier.mabras@protonmail.ch>
2019-07-16 23:29:36 +0200

    Add missing config file for pip

--- setup.py
+++ setup.py
@@ -5,20 +5,17 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cups",
-    version="0.0.1",
+    version="0.0.6",
     author="Xavier Mabras",
     author_email="xavier.mabras@protonmail.ch",
     description="A simple cups generator library",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/xafardero/cups",
-    download_url = 'https://github.com/xafardero/cups/tarball/0.0.1',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    install_requires=[
     ],
 )
