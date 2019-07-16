ac765809a1eb8dec6f11078d75acd152ae0e10b2
xaf <xavier.mabras@protonmail.ch>
2019-07-16 23:29:36 +0200

    Add missing config file for pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cups",
-    version="0.0.1",
+    version="0.0.2",
     author="Xavier Mabras",
     author_email="xavier.mabras@protonmail.ch",
     description="A simple cups generator library",
