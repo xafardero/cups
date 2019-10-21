1df55aec2bf8b51fb88341606050d70098cc5271
Arnau Blanché <arnaublanche@gmail.com>
2019-10-21 14:17:22 +0200

    Remove unnecesary functions and prints on __init__

--- cups/GasCups.py
+++ cups/GasCups.py
@@ -3,8 +3,5 @@ from CupsGenerator import CupsGenerator
 
 class GasCupsGenerator(CupsGenerator):
 
-    def __init__(self):
-        super().__init__()
-
     def _start_with(self) -> string:
         return '02'
--- cups/__init__.py
+++
@@ -1,8 +0,0 @@
-from EnergyCups import EnergyCupsGenerator
-from GasCups import GasCupsGenerator
-
-ExampleEnergyCupsGenerator = EnergyCupsGenerator()
-print('Energy: ' + ExampleEnergyCupsGenerator.generate())
-
-ExampleGasCupsGenerator = GasCupsGenerator()
-print('Gas: ' + ExampleGasCupsGenerator.generate())
